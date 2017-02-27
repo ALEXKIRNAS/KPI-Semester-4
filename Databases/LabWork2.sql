356f5521ce5b9e9009765cf0ba92928c51b5ff38
ALEXKIRNAS <alexkirnas@ukr.net>
2017-02-27 14:12:28 +0200

    Fixing shitcode at LabWork2 at Databases.

--- Databases/LabWork2.sql
+++ Databases/LabWork2.sql
@@ -29,27 +29,27 @@ LEFT JOIN [Northwind].[dbo].[Suppliers] AS [S]
 -- Task 2.2
 SELECT * FROM [Northwind].[dbo].[Orders]
 WHERE ([ShippedDate] IS NULL) AND
-	  ([OrderDate] BETWEEN '1988-04-01' AND '1988-05-01')
---ORDER BY [OrderDate]
+      ([OrderDate] BETWEEN '1998-04-01' AND '1998-05-01') -- Selection of all dates from interval [1998-04-01; 1998-05-01) in MS SQL
 
 -- Task 2.3
-SELECT [LastName]
-      ,[FirstName]
-FROM [Northwind].[dbo].[Employees]
-WHERE [EmployeeID] IN (SELECT [EmployeeID]
-                       FROM [Northwind].[dbo].[EmployeeTerritories]
-                       WHERE [TerritoryID] IN (SELECT [TerritoryID]
-                                               FROM [Northwind].[dbo].[Territories] 
-                                               WHERE [RegionID] IN (SELECT [RegionID] 
-                                                                    FROM [Northwind].[dbo].[Region] 
-                                                                    WHERE [Northwind].[dbo].[Region].[RegionDescription] = 'Northern')))
+SELECT DISTINCT [LastName]
+               ,[FirstName]
+FROM [Northwind].[dbo].[Employees] AS [E]
+LEFT JOIN [Northwind].[dbo].[EmployeeTerritories] AS [ET] 
+          ON [E].[EmployeeID] = [ET].[EmployeeID]
+LEFT JOIN [Northwind].[dbo].[Territories] AS [T] 
+          ON [T].[TerritoryID] = [ET].[TerritoryID]
+LEFT JOIN [Northwind].[dbo].[Region] AS [R] 
+          ON [T].[RegionID] = [R].[RegionID]
+WHERE [R].[RegionDescription] = 'Northern' 
 
 -- Task 2.4
-SELECT SUM([UnitPrice] * (1 - [Discount]) * [Quantity]) AS 'Sum'
+SELECT CAST(SUM([UnitPrice] * (1 - [Discount]) * [Quantity]) AS Money)  AS 'Sum'
 FROM [Northwind].[dbo].[Order Details] AS [OD]
 LEFT JOIN [Northwind].[dbo].[Orders] AS [O] 
           ON [O].[OrderID] = [OD].[OrderID]
 WHERE (DATENAME(d, [O].[OrderDate]) % 2 = 1)
+
 
 -- Task 2.5
 SELECT [ShipAddress]
@@ -58,3 +58,17 @@ WHERE [OrderID] = (SELECT TOP(1) [OrderID]
                    FROM [Northwind].[dbo].[Order Details]
                    GROUP BY [OrderID]
                    ORDER BY SUM([UnitPrice] * (1 - [Discount]) * [Quantity]) DESC)
+
+
+-- Task 2.5
+-- Without sorting
+;WITH [table] as (SELECT [OrderID],
+                  SUM([UnitPrice] * (1 - [Discount]) * [Quantity]) AS 'Sum'
+                  FROM [Northwind].[dbo].[Order Details]
+                  GROUP BY [OrderID])
+
+
+SELECT [ShipAddress]
+FROM [Northwind].[dbo].[Orders]
+WHERE [OrderID] LIKE (SELECT [OrderID] FROM [table] -- Taking OrderID of max value
+                      WHERE [Sum] = (SELECT MAX([Sum]) FROM [table])) -- Finging max value from table
