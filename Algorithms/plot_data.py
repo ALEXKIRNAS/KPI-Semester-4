25de12b202d16dbe68e0da350e8b3431c9eb6a48
ALEXKIRNAS <alexkirnas@ukr.net>
2017-03-08 12:11:28 +0200

    Adding labwork 2 to Algorithms.

--- Algorithms/plot_data.py
+++ Algorithms/plot_data.py
@@ -4,15 +4,23 @@ import collections
 from pylab import figure, show
 from math import log
 
-def plot_data(data, logarithmic=False, oneplot=False):
+def plot_data(data, logarithmic=False, oneplot=False, data_2={}):
 #    print data
     fig = figure(1)
-    num = len(data)
+    num = 0
+    if oneplot==True:
+        num = 1
+        if len(data_2)>0:
+            num += 1
+    else:
+        num = len(data)
+        if len(data_2)>0:
+            num += 1
     colors = ['r','b','g']
     markers = ['s','o','x']
     lines = ['-','--',':']
     if oneplot==True:
-        ax = fig.add_subplot(111)
+        ax = fig.add_subplot(num, 1, 1)
         ax.grid(True)
         i = -1
         line_titles = []
@@ -20,6 +28,7 @@ def plot_data(data, logarithmic=False, oneplot=False):
         for label, value in data.iteritems():
             i += 1
             j = -1
+            sort_num = len(value)
             for sort_type, points in value.iteritems():
                 j += 1
                 od_points = collections.OrderedDict(sorted(points.items()))
@@ -33,7 +42,7 @@ def plot_data(data, logarithmic=False, oneplot=False):
                 x_max = max(x_max, max(xs))
                 ys.insert(0,0)
                 y_max = max(y_max, max(ys))
-                ax.plot(xs, ys, colors[j%num]+markers[j%num]+lines[i%num], label=sort_type )
+                ax.plot(xs, ys, colors[j%sort_num]+markers[j%sort_num]+lines[i%num], label=sort_type )
                 line_titles.append(sort_type+' '+label)
         ax.set_xlim( (0, x_max*1.1) )
         ax.set_ylim( (0, y_max*1.1) )
@@ -64,4 +73,33 @@ def plot_data(data, logarithmic=False, oneplot=False):
             ax.set_xlim( (0, x_max*1.1) )
             ax.set_ylim( (0, y_max*1.1) )
             ax.legend(loc=4)
+            
+    if len(data_2)>0:
+        ax = fig.add_subplot(num,1,num)
+        ax.grid(True)
+        i = -1
+        line_titles = []
+        x_max = y_max = 0
+        for label, value in data_2.iteritems():
+            i += 1
+            j = -1
+            sort_num = len(value)
+            for name, points in value.iteritems():
+                j += 1
+                od_points = collections.OrderedDict(sorted(points.items()))
+                if logarithmic:
+                    xs = [(x>0 and log(x,10) or 0) for x in od_points.keys()]
+                    ys = [(y>0 and log(y,10) or 0) for y in od_points.values()]
+                else:
+                    xs = od_points.keys()
+                    ys = od_points.values()
+                xs.insert(0,0)
+                x_max = max(x_max, max(xs))
+                ys.insert(0,0)
+                y_max = max(y_max, max(ys))
+                ax.plot(xs, ys, colors[j%sort_num]+markers[j%sort_num]+lines[i%num], label=sort_type )
+                line_titles.append(name+' '+label)
+        ax.set_xlim( (0, x_max*1.1) )
+        ax.set_ylim( (0, y_max*1.1) )
+        ax.legend(line_titles, loc=4)
     show()
